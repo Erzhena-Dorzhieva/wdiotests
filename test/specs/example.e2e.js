20ecc087335417e0f1520daf3331cf429c5a7b59
git config --global user.name "Erzhena-Dorzhieva <git config --global user.email "dorzhievaerzhena@gmail.com>
2021-04-02 21:34:13 -0400

    Second commit

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,39 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import  ProfilePage from '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    it('user logs in with valid data', () => {
         LoginPage.open();
+        LoginPage.setLogin('cekak37968@990ys.com');
+        LoginPage.setPassword('123456');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
 
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
     });
 });
 
-
+//import LoginPage from  '../pageobjects/login.page';
+// import ProfilePage from '../pageobjects/profile.page';
+// describe('Auth', () => {
+//     beforeEach(()=>{
+//         LoginPage.open();
+//     })
+//     afterEach(()=>{
+//         browser.execute('window.localStorage.clear()')
+//     })
+//     it('user logs in with valid data', () => {
+//         LoginPage.setLogin('cekak37968@990ys.com');
+//         LoginPage.setPassword('123456');
+//         LoginPage.clickSubmitButton();
+//         ProfilePage.isOpen();
+//     });
+//     it('Submit button is disabled if login and password are absent', ()=>{
+//         LoginPage.submitButtonIsDisabled()
+//     });
+//     it('fails if invalid data provided', ()=>{
+//         LoginPage.setLogin('example@example.com');
+//         LoginPage.setPassword('123123');
+//         LoginPage.clickSubmitButton();
+//         LoginPage.errorToastAppeared();
+//     })
+// });
