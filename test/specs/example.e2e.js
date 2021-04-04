9fdd5bdb6f786382077c22eb5ea5b591c78c0365
git config --global user.name "Erzhena-Dorzhieva <git config --global user.email "dorzhievaerzhena@gmail.com>
2021-04-04 00:41:28 -0400

    Second commit

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,38 +2,54 @@ import LoginPage from  '../pageobjects/login.page';
 import  ProfilePage from '../pageobjects/profile.page';
 
 describe('Auth', () => {
-    it('user logs in with valid data', () => {
+    beforeEach(() => {
         LoginPage.open();
+    });
+
+    afterEach(() => {
+       browser.execute('window.localStorage.clear()');
+    });
+
+    it('user logs in with valid data', () => {
         LoginPage.setLogin('cekak37968@990ys.com');
         LoginPage.setPassword('123456');
         LoginPage.clickSubmitButton();
         ProfilePage.isOpen();
-
     });
-});
 
-//import LoginPage from  '../pageobjects/login.page';
-// import ProfilePage from '../pageobjects/profile.page';
-// describe('Auth', () => {
-//     beforeEach(()=>{
-//         LoginPage.open();
-//     })
-//     afterEach(()=>{
-//         browser.execute('window.localStorage.clear()')
-//     })
-//     it('user logs in with valid data', () => {
-//         LoginPage.setLogin('cekak37968@990ys.com');
-//         LoginPage.setPassword('123456');
-//         LoginPage.clickSubmitButton();
-//         ProfilePage.isOpen();
-//     });
-//     it('Submit button is disabled if login and password are absent', ()=>{
-//         LoginPage.submitButtonIsDisabled()
-//     });
-//     it('fails if invalid data provided', ()=>{
-//         LoginPage.setLogin('example@example.com');
-//         LoginPage.setPassword('123123');
-//         LoginPage.clickSubmitButton();
-//         LoginPage.errorToastAppeared();
-//     })
-// });
+    it('submit button is disabled if login and password are absent',() => {
+        LoginPage.submitButtonIsDisabled();
+    });
+
+    it('fails if invalid data provided', () => {
+       LoginPage.setLogin('example@example.com');
+       LoginPage.setPassword('1234567');
+       LoginPage.clickSubmitButton();
+       LoginPage.errorToastAppeared();
+    });
+
+    // it('fails if invalid password is provided', () => {
+    //     LoginPage.setLogin('cekak37968@990ys.com');
+    //     LoginPage.setPassword('123456');
+    //     LoginPage.clickSubmitButton();
+    //     LoginPage.errorToastAppeared();
+    // });
+    //
+    // it('fails if invalid email is provided', () => {
+    //     LoginPage.setLogin('example@example.com');
+    //     LoginPage.setPassword('1234567');
+    //     LoginPage.clickSubmitButton();
+    //     LoginPage.errorToastAppeared();
+    // });
+    it.only('login input is required', () => {
+        LoginPage.setLogin('example@example.com');
+        LoginPage.deleteLogin();
+        LoginPage.loginRequiredError();
+    });
+
+    // it('password input is required', () => {
+    //     LoginPage.setPassword('1234567');
+    //     LoginPage.deletePassword();
+    //     LoginPage.errorToastAppeared();
+    // });
+});
